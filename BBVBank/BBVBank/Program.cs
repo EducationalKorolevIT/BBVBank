9c88ddf571e444f7d84be8c6dd39c91a1ecb8f72
Stepan358 <59833944+Stepan358@users.noreply.github.com>
2020-01-27 21:27:23 +0300

    Доделал меню.
    123

--- BBVBank/BBVBank/Program.cs
+++ BBVBank/BBVBank/Program.cs
@@ -7,20 +7,78 @@ namespace BBVBank
         static void Main(string[] args)
         {
             int m = 1;
-            char key = Console.ReadKey(true).KeyChar;
-            Console.WriteLine("--> Создать аккаунт");
-            if (key == 'w')
+            bool Work = true;
+            Console.WriteLine("--> Открыть счёт" + " \nВнести деньги" + "\nСнять деньги" + "\nПеревести деньги" + "\nВзять кредит" + "\nПомощь" + "\nАвторы");
+            while (Work == true)
             {
-                m++;
-            }
-            if(m == 1)
-            {
-                Console.Clear();
-                Console.WriteLine("--> Создать аккаунт");
-            }
-            else
-            {
-                Console.WriteLine("Создать аккаунт");
+                char key = Console.ReadKey(true).KeyChar;
+                if (key == 'w')
+                {
+                    m++;
+                }
+   
+                if (m == 8)
+                {
+                    m = 1;
+                }
+                if (m == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("--> Открыть Счёт");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Открыть Счёт");
+                }
+                if (m == 2)
+                {
+                    Console.WriteLine("--> Внести деньги");
+                }
+                else
+                {
+                    Console.WriteLine("Внести деньги");
+                }
+                if (m == 3)
+                {
+                    Console.WriteLine("--> Снять деньги");
+                }
+                else
+                {
+                    Console.WriteLine("Снять деньги");
+                }
+                if (m == 4)
+                {
+                    Console.WriteLine("--> Перевести деньги");
+                }
+                else
+                {
+                    Console.WriteLine("Перевести деньги");
+                }
+                if (m == 5)
+                {
+                    Console.WriteLine("--> Взять кредит");
+                }
+                else
+                {
+                    Console.WriteLine("Взять кредит");
+                }
+                if (m == 6)
+                {
+                    Console.WriteLine("--> Помощь");
+                }
+                else
+                {
+                    Console.WriteLine("Помощь");
+                }
+                if (m == 7)
+                {
+                    Console.WriteLine("--> Авторы");
+                }
+                else
+                {
+                    Console.WriteLine("Авторы");
+                }
             }
         }
     }
