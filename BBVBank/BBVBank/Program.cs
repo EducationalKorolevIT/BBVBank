d7598acfe998c5290f4eb85436e62820deb17af8
Stepan <59833944+Stepan358@users.noreply.github.com>
2020-01-27 16:38:37 +0300

    123

---
+++ BBVBank/BBVBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BBVBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = 1;
+            char key = Console.ReadKey(true).KeyChar;
+            Console.WriteLine("--> Создать аккаунт");
+            if (key == 'w')
+            {
+                m++;
+            }
+            if(m == 1)
+            {
+                Console.Clear();
+                Console.WriteLine("--> Создать аккаунт");
+            }
+            else
+            {
+                Console.WriteLine("Создать аккаунт");
+            }
+        }
+    }
+}
